55ad1bf365cb51632ff3c4dd4fbe6f0894e4a17e
Didi Dodson <mdodson55@gmail.com>
2021-07-23 17:07:18 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+famous_person = "Bob Ross"
+is_active = false
+famous_person = "Dave Grohl"
+
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+puts sum
+
+animals = ['wombat', 'walrus', 'whale']
+animals.shift
+
+puts famous_person.length
