d2e81d3ab03b2702c098f89d8729819fdc51bcbc
Didi Dodson <mdodson55@gmail.com>
2021-07-23 17:12:46 -0600

    Add school model hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+bob_ross_high_school = {
+  "mascot" => "The Paintbrushes",
+  "city" => "Arvada",
+  "number_of_students" => 400,
+  "number_of_teachers" => 40,
+  "kindergarten" => false,
+  "gym_class" => true,
+  "after_school_sports" => ['baseball', 'curling', 'sumo'],
+  "favorite_classes" => ['drama', 'art', 'biology']
+}
