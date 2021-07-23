530ba7093049986c709fb744ca6a63e302e1d295
Didi Dodson <mdodson55@gmail.com>
2021-07-23 17:17:33 -0600

    Add pet model hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+rick_the_cat = {
+  "color" => "black",
+  "favorite_food" => "tuna",
+  "age_in_yrs" => 5,
+  "weight_in_lbs" => 26,
+  "plays_with_toys" => false,
+  "hunts_bugs" => true,
+  "features" => ['furry ears', 'yellow eyes', 'black nose'],
+  "sounds" => ['yoowwwlll', 'brrrppbb', 'purrr']
+}
